0f4f451a4c82ea38ff636c2cc7f7f10e2926deaf
logic-fabric <lowic@logic-fabric.fr>
2021-11-25 05:04:53 +0100

    devtools: add React-Router and Material-UI libraries

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from "@material-ui/core/Button";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <Button variant="contained">I'm a Material-UI Button!</Button>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
